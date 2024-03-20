9b53c6a730c5909f351ca072ed318d5c444c4c8c
Akanksha Garg <akankshagarg@Akankshas-MacBook-Pro.local>
2024-03-20 09:19:32 +0530

    resolving conflicts

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -4,8 +4,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Header = () => {
   return (
-    <div className="header relative">
-      <div className="header-bar mt-10 flex justify-between p-[10px] absolute z-[100] left-[25%] top-[5%] rounded-[50px] md:min-w-[900px]">
+    <div className="header relative ">
+      <div className="header-bar flex justify-between p-[10px] absolute z-[100] md:left-[25%] top-[5%] rounded-[50px] md:min-w-[900px]">
         <div className="header-logos flex flex-row">
           <div className="p-4">
             <Image src="/assets/blog/home.png" height={40} width={40} alt="home-logo"></Image>
@@ -14,7 +14,7 @@ const Header = () => {
             <Image src="/assets/blog/Jeap-transparent.png" height={180} width={180} alt="home-logo"></Image>
           </div>
         </div>
-        <div className="header-links flex flex-row gap-8 p-5">
+        <div className="header-links flex flex-row gap-4 md:gap-8 md:p-5">
         <h2 className="font-semibold text-white">LEADERSHIP</h2>
         <h2 className="font-semibold text-white">CONTACT US</h2>
         <h2 className="font-semibold text-white">RESOURCES</h2>
