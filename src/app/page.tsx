f6933d83e311b81384391cb70402b9e447d97677
dharm <singhdharm1008@gmail.com>
2024-04-02 09:09:59 +0530

    operations updated for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Index() {
         <TurnAroundTime />
         <Tabs />
         <TimeLine />
-        {/* <Operation /> */}
+        <Operation />
         <Resources />
         <Leadership />
       </ErrorBoundary>
