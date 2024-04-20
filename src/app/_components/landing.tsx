262692b3eb8e62829b1600b544207d804cb5be28
dharm <singhdharm1008@gmail.com>
2024-04-20 11:48:08 +0530

    scroll transition and bg fade out effect

--- src/app/_components/landing.tsx
+++ src/app/_components/landing.tsx
@@ -1,21 +1,18 @@
 "use client";
 export default function Landing({ isVisible }: { isVisible: boolean }) {
   return (
-    <div className="min-h-screen fixed">
-      {isVisible ? null : (
-        <section
-          className={`relative text-white w-full h-screen bg-cover bg-center bg-opacity-80 flex flex-col justify-center items-center [clip-path:circle(75%_at_49%_29%)] md:[clip-path:circle(180vh_at_50%_-80vh)] animate-[changeImage_50s_linear_infinite]
-        `}
-        >
-          <div className="flex flex-col justify-center items-center w-[95%] text-center">
-            <h1 className="leading-1 md:leading-[1.5] text-3xl md:text-6xl font-[compasse-extrabold]">THE JOINT EMERGENCY ACTION PLAN (JEAP) UNLOCKING AFRICA'S RESILIENCE</h1>
-            <p className="text-lg md:text-3xl">
-              The JEAP, borne out of a partnership between the Africa CDC and the World Health Organization, is a regional platform that focuses on consolidating Pandemic Preparedness and Response
-              (PPR) efforts across the continent to dramatically improve how countries prepare for, detect, and respond to emergencies, while simultaneously ensuring no country is left behind.
-            </p>
-          </div>
-        </section>
-      )}
+    <div className={`min-h-screen fixed transition-opacity ${isVisible ? "opacity-0" : "opacity-100"}`}>
+      <section
+        className={`relative text-white w-full h-screen bg-cover bg-center bg-opacity-80 flex flex-col justify-center items-center [clip-path:circle(75%_at_49%_29%)] md:[clip-path:circle(180vh_at_50%_-80vh)] animate-[changeImage_50s_linear_infinite]`}
+      >
+        <div className="flex flex-col justify-center items-center w-[95%] text-center">
+          <h1 className="leading-1 md:leading-[1.5] text-3xl md:text-6xl font-[compasse-extrabold]">THE JOINT EMERGENCY ACTION PLAN (JEAP) UNLOCKING AFRICA'S RESILIENCE</h1>
+          <p className="text-lg md:text-3xl">
+            The JEAP, borne out of a partnership between the Africa CDC and the World Health Organization, is a regional platform that focuses on consolidating Pandemic Preparedness and Response (PPR)
+            efforts across the continent to dramatically improve how countries prepare for, detect, and respond to emergencies, while simultaneously ensuring no country is left behind.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
