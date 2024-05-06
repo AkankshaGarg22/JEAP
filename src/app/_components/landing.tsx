444e014060f1aacded3d1d7f872a16011d22044c
Priyanshu Bir <80509195+sauronium@users.noreply.github.com>
2024-05-06 22:41:30 +0530

    Parallax effect rectified

--- src/app/_components/landing.tsx
+++ src/app/_components/landing.tsx
@@ -7,6 +7,7 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 export default function Landing({ isVisible }: { isVisible: boolean }) {
 
     const parallaxRef = useRef<HTMLDivElement>(null);
+    const parrallaxRef = useRef<HTMLDivElement>(null);
   
     useEffect(() => {
       gsap.registerPlugin(ScrollTrigger);
@@ -23,13 +24,28 @@ export default function Landing({ isVisible }: { isVisible: boolean }) {
       });
     }, []);
 
+    useEffect(() => {
+      gsap.registerPlugin(ScrollTrigger);
+  
+      gsap.to(parrallaxRef.current, {
+        backgroundPositionY: '-150px', // Adjust the movement value as needed
+        ease: 'none',
+        scrollTrigger: {
+          trigger: parrallaxRef.current,
+          start: 'top center',
+          end: 'bottom center',
+          scrub: true,
+        }
+      });
+    }, []);
+
 
   return (
     <div className={` min-h-screen fixed transition-opacity scroll-background`} ref={parallaxRef}>
       
       <section
         className={`relative text-white w-full h-screen bg-cover bg-no-repeat bg-center bg-opacity-80 flex flex-col justify-center items-center [clip-path:circle(75%_at_49%_29%)] md:[clip-path:circle(180vh_at_50%_-80vh)]  
-        animate-[changeImage_50s_linear_infinite]`} 
+        animate-[changeImage_50s_linear_infinite]`} ref={parrallaxRef}
       >
         <div className="md:pt-[200px] flex flex-col justify-center items-center w-[90%] xl:w-[60%] text-center">
           <h1 className="leading-1 md:leading-[1.5] text-3xl md:text-6xl font-[compasse-extrabold]">THE JOINT EMERGENCY ACTION PLAN (JEAP) UNLOCKING AFRICA'S RESILIENCE</h1>
