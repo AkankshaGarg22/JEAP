4a55bee3d7afd6046ce74861f1830cb8fb0aa8df
dharm <singhdharm1008@gmail.com>
2024-05-20 23:53:43 +0530

    mission framer added

--- src/app/_components/mission.tsx
+++ src/app/_components/mission.tsx
@@ -1,17 +1,35 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
 
 const Mission = ({ isVisible }: { isVisible: boolean }) => {
-  console.log({isVisible})
+  const [isCurved, setIsCurved] = useState(true);
 
-  // ${isVisible ? "rounded-none" : "[clip-path:circle(50%_at_50%_0%)]"}
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (ref.current) {
+        const scrollTop = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        const { top } = ref.current.getBoundingClientRect();
+        const distanceFromTop = top + scrollTop;
+        setIsCurved(distanceFromTop <= window.innerHeight * 0.1); // Set isCurved to false when the div is 90% away from the top
+      }
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div
+    <motion.div
       id="mission"
-      className={
-        `min-h-screen flex flex-col items-center justify-start xl:justify-evenly bg-cover bg-center lg:bg-contain bg-no-repeat  z-[200] 
-        bg-[#1a5632] text-white ${isVisible ? "rounded-none" : "rounded-t-[669px]"}
+      ref={ref}
+      initial={{ clipPath: "circle(100vh at 49% 100%)" }}
+      animate={{ clipPath: isCurved ? "circle(100vh at 49% 100%)" : "polygon(0 0, 100% 0, 100% 100%, 0 100%)" }}
+      transition={{ duration: 1, type: "easeInOut" }}
+      className={`min-h-screen flex flex-col items-center justify-start xl:justify-evenly bg-cover bg-center lg:bg-contain bg-no-repeat z-[200] bg-[#1a5632] text-white 
         `}
     >
       <div className="basis-2/3 md:basis-2/3 flex flex-col items-center justify-center md:justify-center xl:justify-center md:mt-4 gap-2 xl:gap-4 text-center  md:w-3/4 md:pt-4 ">
@@ -24,7 +42,7 @@ const Mission = ({ isVisible }: { isVisible: boolean }) => {
           </p>
         </div>
       </div>
-      <div className="basis-1/3 md:basis-1/3 flex items-center text-center w-2/3 md:w-1/3 text-wrap text-lg md:text-xl xl:text-3xl font-bold  md:pt-0">
+      <div className="basis-1/3 md:basis-1/3 xl:min-h-96 flex items-center text-center w-2/3 md:w-1/3 text-wrap text-lg md:text-xl xl:text-3xl font-bold  md:pt-0">
         <Typewriter
           onInit={(typewriter) => {
             typewriter
@@ -41,7 +59,7 @@ const Mission = ({ isVisible }: { isVisible: boolean }) => {
           options={{ loop: false }}
         />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
