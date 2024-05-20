ae88eb946cc8dc7ac0b028643594acbd4258a8e6
AkankshaGarg22 <garg.akanksha002@gmail.com>
2024-05-20 20:32:59 +0530

    adding changes for height

--- src/app/_components/leadership.tsx
+++ src/app/_components/leadership.tsx
@@ -35,7 +35,7 @@ const LeadersArray = [
 export function Leadership() {
     return (
         <div className="flex flex-col items-center xl:px-16">
-            <h1 className="font-[compasse-extrabold] p-5 text-black md:text-7xl text-3xl md:p-16">LEADERSHIP</h1>
+            <h1 className="font-[compasse-extrabold] p-5 text-black md:text-7xl text-3xl md:pb-16 md:pt-8">LEADERSHIP</h1>
             <div id="leadership" className="flex flex-col xl:grid xl:grid-cols-3 xl:grid-rows-2 xl:gap-0 xl:gap-y-6 sm:items-center sm:gap-6 gap-2 justify-evenly w-full">
                 {LeadersArray.map((leader, index) => {
                     return leaderCard(leader, index)
@@ -49,9 +49,9 @@ const leaderCard = (leaderData: { image: string, title: string, description: str
     return(
      <div className="leader min-h-full flex flex-col items-center gap-6 justify-center" key={key}>
         <Image className="image-hover max-h-[180px] max-w-[180px] md:max-h-[300px] md:max-w-[300px] rounded-full" src={leaderData.image} width={300} height={0} alt={leaderData.title + 'image'}></Image>
-        <div className="flex flex-col items-center md:pt-8">
+        <div className="flex flex-col items-center">
             <h2 className="text-[#00205C] text-base md:text-lg font-bold flex text-center">{leaderData.title}</h2>
-            <p className="text-[#00205C] text-base md:text-lg p-4 flex text-center">{leaderData.description}</p>
+            <p className="text-[#00205C] text-base md:text-lg p-1 flex text-center">{leaderData.description}</p>
         </div>
     </div>)
 }
