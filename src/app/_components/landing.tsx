d5802cf8654c79eb7105f58f968ece9bdbe4e0ea
Priyanshu Bir <80509195+sauronium@users.noreply.github.com>
2024-05-21 00:24:35 +0530

    Text wrapping updated

--- src/app/_components/landing.tsx
+++ src/app/_components/landing.tsx
@@ -147,7 +147,7 @@ export default function Landing({ isVisible }: { isVisible: boolean }) {
        }}
       >
         <div className="xl:pt-[200px] flex flex-col justify-center items-center w-[90%] xl:w-[60%] text-center">
-          <h1 className="leading-1 md:leading-[1.5] text-3xl md:text-6xl font-[compasse-extrabold] leading-tight">THE JOINT EMERGENCY ACTION PLAN (JEAP) UNLOCKING AFRICA'S RESILIENCE</h1>
+          <h1 className="xl:leading-[1] text-3xl md:text-6xl font-[compasse-extrabold] leading-tight">THE JOINT EMERGENCY ACTION PLAN (JEAP) UNLOCKING AFRICA'S RESILIENCE</h1>
           <p className="text-lg md:text-2xl text-balance pt-5">
             The JEAP, borne out of a partnership between the Africa CDC and the World Health Organization, is a regional platform that focuses on consolidating Emergency Preparedness and Response (EPR)
             efforts across the continent to dramatically improve how countries prepare for, detect, and respond to emergencies, while simultaneously ensuring no country is left behind.
--- src/app/_components/tabs.tsx
+++ src/app/_components/tabs.tsx
@@ -71,7 +71,7 @@ export function Tabs() {
     <div id="vision" className="min-h-screen lg:min-h-full flex flex-col items-center justify-evenly md:justify-normal lg:block">
       <div className="text-center lg:my-2">
         <h2 className="text-[#000] text-3xl md:text-6xl xl:text-7xl font-[compasse-extrabold] my-4">THE JEAP VISION</h2>
-        <p className="text-xl xl:px-0 py-2 xl:py-4">
+        <p className="text-xl xl:px-0 py-2 xl:py-4 text-balance">
           At the heart of the JEAP is a vision of 'One Africa for Health Security.' This vision outlines six collaboration areas and provides a comprehensive framework for coordinated action, to
           safeguard African populations from public health emergencies.
         </p>
