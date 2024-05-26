68f550e8df4b08a82b2e353b455b2abb37d87a30
Priyanshu Bir <80509195+sauronium@users.noreply.github.com>
2024-05-26 17:32:51 +0530

    Landing text update

--- src/app/_components/landing.tsx
+++ src/app/_components/landing.tsx
@@ -172,7 +172,7 @@ export default function Landing({ isVisible }: { isVisible: boolean }) {
         <div className="xl:pt-[200px] flex flex-col justify-center items-center w-[90%] xl:w-[60%] text-center">
           <h1 className="xl:leading-[1] text-3xl md:text-6xl font-[compasse-extrabold] leading-tight">THE JOINT EMERGENCY ACTION PLAN (JEAP)</h1>
           <h1 className="xl:leading-[1] text-3xl md:text-6xl font-[compasse-extrabold] leading-tight">UNLOCKING AFRICA'S RESILIENCE</h1>
-          <p className="text-lg md:text-2xl w-[100%] sm:leading-none sm:px-4 md:w-[80%] xl:w-[80%] text-balance break-normal">
+          <p className="text-lg md:text-2xl w-[100%] sm:leading-none sm:px-4 md:pt-5 md:w-[80%] xl:pt-6 xl:w-[80%] text-balance break-normal">
             The JEAP, borne out of a partnership between the Africa CDC and the World Health Organization, is a regional platform that focuses on consolidating Emergency Preparedness and Response (EPR)
             efforts across the continent to dramatically improve how countries prepare for, detect, and respond to emergencies, while simultaneously ensuring no country is left behind.
           </p>
