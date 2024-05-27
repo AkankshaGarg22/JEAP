d87b9876a960fca10770cabc16d4a42e47ea536f
dharm <singhdharm1008@gmail.com>
2024-05-27 19:03:57 +0530

    preload images landing and timeline

--- src/app/_components/time-line.tsx
+++ src/app/_components/time-line.tsx
@@ -6,6 +6,7 @@ import { PopUpState, TimeLineItem } from "@/interfaces/timeline";
 import { gsap } from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import Head from "next/head";
 
 // const backgroundImageUrl = "/assets/blog/jpgs/WHO_Ghana-151560.webp";
 
@@ -23,8 +24,8 @@ export const items: TimeLineItem[] = [
     cardSubtitle: "Conceptualizing the JEAP",
     cardDetailedText:
       "In response to the challenges posed by COVID-19, Africa CDC and WHO joined forces to revolutionize Emergency Preparedness and Response (EPR) across the continent. A joint Steering Committee was established, driving collective efforts to enhance response activities for improved coverage and impact.",
-    minHeight: "45%"
-    },
+    minHeight: "45%",
+  },
   {
     key: 2,
     year: "2023",
@@ -32,8 +33,8 @@ export const items: TimeLineItem[] = [
     cardSubtitle: "Unveiling the JEAP",
     cardDetailedText:
       "The JEAP was launched during the 2023 World Health Assembly, uniting partners to address critical challenges in preparedness, detection, and response across six programmatic areas. AVoHC-SURGE, a collaborative workforce initiative jointly led by Africa CDC and WHO, enables African nations to respond rapidly (within 24-48 hours) to health emergencies and humanitarian crises. It welcomes stakeholders from humanitarian, development, and government agencies, ensuring life-saving care reaches all. So far, 16 countries have utilized AVoHC-SURGE members for local responses (Zanzibar, United Republic of Tanzania, Togo, Senegal, Rwanda, Republic of the Congo, Nigeria, Niger, Namibia, Mauritania, Malawi, Kenya, Ethiopia, Democratic Republic of Congo, Chad, Central African Republic), while 6 countries have deployed them internationally (Ethiopia, Tanzania, Rwanda, DRC, Republic of Congo, Botswana).",
-    minHeight: "45%"
-    },
+    minHeight: "45%",
+  },
   {
     key: 3,
     year: "2023 - 2027",
@@ -41,8 +42,8 @@ export const items: TimeLineItem[] = [
     cardSubtitle: "The Future of the JEAP",
     cardDetailedText:
       "Operating under the Africa CDC - WHO Memorandum of Understanding (MoU), the JEAP emerges as a promising regional framework that can be leveraged for catalytic and inclusive improvements in pandemic preparedness and response.\n Investments in a consolidated, regional pandemic investment plan such as the JEAP are needed to support countries mitigate the impact of future health shocks and empower nations to protect their citizens from emerging health threats.",
-    minHeight: "33%"
-    },
+    minHeight: "33%",
+  },
 ];
 
 export function TimeLine() {
@@ -152,30 +153,37 @@ export function TimeLine() {
   };
 
   return (
-    <div id="jeap-journey" className="h-full flex flex-col">
-      <div className="text-center md:pb-[10px]">
-        <h2 className="text-[#000000] text-3xl md:text-7xl font-[compasse-extrabold] my-4">THE JEAP JOURNEY</h2>
-      </div>
-      <div
-        className="relative bg-cover bg-center w-full p-4 transition-bg-image duration-500 ease-in-out"
-        style={{ backgroundImage: `url(${backgroundImage}),url(${bgImages[1].imageUrl}),url(${bgImages[2].imageUrl})` }}
-      >
-        {/* <div className="absolute inset-0 bg-gradient-to-br from-[#00205C] to-[#1A5632] opacity-80" /> */}
-        <div className="timeline my-4 text-transparent ">
-          <div className="line"></div>
-          {items.map((item) => (
-            <div id={`section-${item.key}`} className={`section text-wrap text-3xl w-3/4 md:w-3/4 ${activeSectionId === `section-${item.key}` ? "active" : ""}`} key={item.key}>
-              <div className="section-ball absolute -top-2 -left-2 w-4 h-4 bg-white rounded-full cursor-pointer" onClick={(e) => handleClick(item.key, e)}></div>
-              <div className="section-title text-white">{item.year}</div>
-              <h2 className="text-xl font-bold">{item.cardTitle}</h2>
-              <h5 className="text-xl leading-tight">{item.cardSubtitle}</h5>
-              <p className="hidden md:block md:text-lg transition-opacity ease-in duration-700 opacity-100">{item.cardDetailedText}</p>
-              <button onClick={() => setPopUp({ isOpen: true, item: item })}>Read More</button>
-            </div>
-          ))}
+    <>
+      <Head>
+        {bgImages.map((image) => (
+          <link key={image.id} rel="preload" href={image.imageUrl} as="image" />
+        ))}
+      </Head>
+      <div id="jeap-journey" className="h-full flex flex-col">
+        <div className="text-center md:pb-[10px]">
+          <h2 className="text-[#000000] text-3xl md:text-7xl font-[compasse-extrabold] my-4">THE JEAP JOURNEY</h2>
         </div>
-        {popUp.isOpen && <Modal setPopUp={setPopUp} popUp={popUp} />}
+        <div
+          className="relative bg-cover bg-center w-full p-4 transition-bg-image duration-500 ease-in-out"
+          style={{ backgroundImage: `url(${backgroundImage}),url(${bgImages[1].imageUrl}),url(${bgImages[2].imageUrl})` }}
+        >
+          {/* <div className="absolute inset-0 bg-gradient-to-br from-[#00205C] to-[#1A5632] opacity-80" /> */}
+          <div className="timeline my-4 text-transparent ">
+            <div className="line"></div>
+            {items.map((item) => (
+              <div id={`section-${item.key}`} className={`section text-wrap text-3xl w-3/4 md:w-3/4 ${activeSectionId === `section-${item.key}` ? "active" : ""}`} key={item.key}>
+                <div className="section-ball absolute -top-2 -left-2 w-4 h-4 bg-white rounded-full cursor-pointer" onClick={(e) => handleClick(item.key, e)}></div>
+                <div className="section-title text-white">{item.year}</div>
+                <h2 className="text-xl font-bold">{item.cardTitle}</h2>
+                <h5 className="text-xl leading-tight">{item.cardSubtitle}</h5>
+                <p className="hidden md:block md:text-lg transition-opacity ease-in duration-700 opacity-100">{item.cardDetailedText}</p>
+                <button onClick={() => setPopUp({ isOpen: true, item: item })}>Read More</button>
+              </div>
+            ))}
+          </div>
+          {popUp.isOpen && <Modal setPopUp={setPopUp} popUp={popUp} />}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
