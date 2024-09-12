19fdb4e584e9edec2046eeacde1561b95885a487
AkankshaGarg22 <garg.akanksha002@gmail.com>
2024-09-12 11:43:11 +0530

    Adding changes for new tabs

--- src/app/_components/landing.tsx
+++ src/app/_components/landing.tsx
@@ -153,7 +153,7 @@ export default function Landing({ isVisible }: { isVisible: boolean }) {
             <h1 className="xl:leading-[1] text-3xl md:text-6xl font-[compasse-extrabold] leading-tight">THE JOINT EMERGENCY ACTION PLAN (JEAP)</h1>
             <h1 className="xl:leading-[1] text-3xl md:text-6xl font-[compasse-extrabold] leading-tight">UNLOCKING AFRICA'S RESILIENCE</h1>
             <p className="text-lg md:text-2xl w-[100%] sm:leading-none sm:px-4 md:pt-5 md:w-[80%] xl:pt-6 xl:w-[80%] text-balance break-normal">
-              The JEAP, borne out of a partnership between the Africa CDC and the World Health Organization, is a regional platform that focuses on consolidating Emergency Preparedness and Response
+              The JEAP, born out of a partnership between the Africa CDC and the World Health Organization, is a regional platform that focuses on consolidating Emergency Preparedness and Response
               (EPR) efforts across the continent to dramatically improve how countries prepare for, detect, and respond to emergencies, while simultaneously ensuring no country is left behind.
             </p>
           </div>
