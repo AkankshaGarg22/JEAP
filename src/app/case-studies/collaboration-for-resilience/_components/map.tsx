0e9de0a7c7b4953abcf1967990cdc6e9b8fdb138
dharm <singhdharm1008@gmail.com>
2025-03-31 21:44:30 +0530

    map responsive

--- src/app/case-studies/collaboration-for-resilience/_components/map.tsx
+++ src/app/case-studies/collaboration-for-resilience/_components/map.tsx
@@ -1,7 +1,7 @@
 export default function Map() {
   return (
-    <section className="max-w-6xl mx-auto px-2 md:px-12 mb-12">
-      <div className="flex flex-col md:flex-row items-center justify-center md:justify-between gap-4 md:gap-0 mb-4">
+    <section className="max-w-6xl mx-auto mb-12">
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-between gap-4 md:gap-0 mb-4 px-2 md:px-12">
         <div className="md:w-[50%] h-full flex flex-col justify-between">
           <h3 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4">MAP OF COUNTRIES THAT HAVE SUBMITTED PROPOSALS WITH JEAP SUPPORT:</h3>
           <div className="w-16 h-1 bg-black mb-6 lg:mb-12"></div>
