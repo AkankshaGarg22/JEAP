fe3af526b320f6239dd849ffe6f228555de636a2
AkankshaGarg22 <garg.akanksha002@gmail.com>
2025-03-31 21:11:23 +0530

    adding logistic case study

---
+++ src/app/case-studies/enhancing-operations-support-and-logistics/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "../../_components/layout/footer";
+import Landing from "./_components/landing";
+import OslApproach from "./_components/osl-approach";
+import Partnership from "./_components/partnership";
+
+export default function Index() {
+  return (
+    <div>
+      <Landing />
+      <Partnership />
+      <OslApproach />
+      <Footer />
+    </div>
+  );
+}
