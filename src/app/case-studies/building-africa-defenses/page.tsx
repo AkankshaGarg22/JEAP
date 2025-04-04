644f5dc835069fbc65b7d516519da5f3e53f8464
Priyanshu Bir <80509195+sauronium@users.noreply.github.com>
2025-04-04 05:37:15 +0530

    building africa defenses updated

--- src/app/case-studies/building-africa-defenses/page.tsx
+++ src/app/case-studies/building-africa-defenses/page.tsx
@@ -5,8 +5,35 @@ import TextSectionOne from "./_components/textSectionOne";
 import { BlockQuote } from "../_components/QuoteSection";
 import TextSectionTwo from "./_components/textSectionTwo";
 import TextSectionThree from "./_components/textSectionThree";
-import AnimatedHeading from "../_components/AnimatedHeading";
+import TextSectionFour from "./_components/textSectionFour";
+import TextSectionFive from "./_components/textSectionFive";
+import ReferenceList from "../_components/References";
+import MainHeading from "../_components/main-heading";
+import OverflowSection from "./_components/OverflowSection";
 
+const references = [
+  " https://reliefweb.int/report/mozambique/weekly-bulletin-outbreaks-and-other-emergencies-week-42-16-october-22-october-2023-data-reported-1700-22-october-2023",
+  " WHO Africa, 2022. Available from: https://www.afro.who.int/news/who-partners-seek-reboot-africas-health-emergency-response",
+  " Mremi IR, George J, Rumisha SF, Sindato C, Kimera SI, Mboera LEG. Twenty years of integrated disease surveillance and response in Sub-Saharan Africa: challenges and opportunities for effective management of infectious disease epidemics. One Health Outlook. 2021 Nov 9;3(1):22. doi: 10.1186/s42522-021-00052-9. PMID: 34749835; PMCID: PMC8575546.Available here: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8575546/",
+  " WHO Africa, 2019. “As risks of outbreaks grow, African health ministers agree to new response strategy” Accessed here: https://www.afro.who.int/news/risks-outbreaks-grow-african-health-ministers-agree-new-response-strategy ",
+  " Electronic IHR States Parties Self-Assessment Annual Reporting Tool, WHO, 2022. Access at: https://extranet.who.int/e-spar#capacity-score.",
+  " US CDC. https://www.cdc.gov/coronavirus/2019-ncov/variants/genomic-surveillance.html ",
+  " WHO, 2016a, Accessed at: https://www.undrr.org/understanding-disaster-risk/terminology/hips/bi0062",
+  " Moon S, Sridhar D, Pate MA, Jha AK, Clinton C, Delaunay S, Edwin V, Fallah M, Fidler DP, Garrett L, Goosby E, Gostin LO, Heymann DL, Lee K, Leung GM, Morrison JS, Saavedra J, Tanner M, Leigh JA, Hawkins B, Woskie LR, Piot P. Will Ebola change the game? Ten essential reforms before the next pandemic. The report of the Harvard-LSHTM Independent Panel on the Global Response to Ebola. Lancet. 2015 Nov 28;386(10009):2204-21. doi: 10.1016/ S0140-6736(15)00946-0. Epub 2015 Nov 23. PMID: 26615326; PMCID: PMC7137174.Accessed at: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7137174/",
+  " Moyo E, Moyo P, Mashe T, Dzobo M, Chitungo I, Dzinamarira T. Implementation of Public Health Genomics in Africa: Lessons from the COVID-19 pandemic, challenges, and recommendations. J Med Virol. 2023 Jan;95(1):e28295. doi: 10.1002/jmv.28295. Epub 2022 Nov 17. PMID: 36366938; PMCID: PMC9877907.Accessed at: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9877907/ ",
+  " https://gh.bmj.com/content/4/4/e001427",
+  " https://gh.bmj.com/content/4/4/e001427",
+  " Mremi, I.R., George, J., Rumisha, S.F. et al. Twenty years of integrated disease surveillance and response in Sub-Saharan Africa: challenges and opportunities for effective management of infectious disease epidemics. One Health Outlook 3, 22 (2021). https://doi.org/10.1186/s42522-021-000529 ",
+  " https://www.africakicksoutwildpolio.com/ ",
+  " https://www.afro.who.int/news/how-wastewater-surveillance-helping-covid-19-fight",
+  " WHO Africa 2021. Accessed at: https://www.afro.who.int/news/scaling-genomic-sequencing-africa ",
+  " https://africacdc.org/news-item/a-six-year-journey-advancing-pathogen-genomics-in-africa/ ",
+  " https://africacdc.org/news-item/digitizing-public-health-genomics-in-africa/ ",
+  " WHO. (2022). Accessed at: https://www.who.int/news/item/16-09-2022-reflecting-on-the-implementation-of-genomic-surveillance-for-COVID-19-and-beyond-in-the-african-region",
+  " WHO EMRO | Genomic sequencing for epidemic and pandemic preparedness and response: EMRO’s vision and strategic interventions | Volume 28 issue 12 | EMHJ volume 28 2022",
+  " https://www.who.int/initiatives/genomic-surveillance-strategy",
+
+];
 
 export default function Index() {
   return (
@@ -21,31 +48,28 @@ export default function Index() {
         />
       <TextSectionTwo/>
       <TextSectionThree/>
-      <div className="w-full bg-white py-8"></div>
+      <OverflowSection/>
       <BlockQuote
           quote="Through jeap, we are leveraging all the insights and advancements gained during the covid-19 pandemic to effectively respond to every health situation and event we encounter."
           author="Dr Nicksy Gumede-Moeletsi"
           position="Senior Virologist,"
           organization="WHO Regional Office for Africa"
         />
-
-      <div className="bg-white flex w-full flex-col items-center justify-center py-10">
-        <p className="py-2 font-bold w-[80%]">The close working partnership between Africa CDC and WHO allows for highly coordinated and complementary support to the network. Africa CDC provides technical training, sources equipment, and is developing a real-time data-sharing platform to strengthen the capacity of the network. WHO AFRO, through its country offices, acts as a broker, facilitating agreements, navigating customs, and organizing logistics to ensure samples arrive cold and intact for processing. This strong partnership has enabled the establishment of a network of 13 Centers of Excellence (CoEs) serving as sequencing hubs across the continent.</p> <p className="py-2 font-bold w-[80%]"> Underpinning these efforts, there is a need for strong and harmonious laboratory systems overall. The JEAP has supported the strengthening of the broad laboratory system in Africa by developing a continental guideline for sample referral and ensuring a coordinated network for transporting samples for confirmation of priority pathogens. In November 2023, the Africa CDC introduced a laboratory network information management system to monitor and facilitate informed decision-making on network operations.17</p>
+      <TextSectionFour />
+      <BlockQuote
+          quote="In the face of ongoing global health challenges, The Africa CDC and WHO are committed to working together through the pathogen genomics initiativve to enhance disease outbreak detection and data sharing across Africa strengthening Africa’s genomic sequencing capabilities is essential for regional and global health security"
+          author="Dr. Sofonias Tessema,"
+          position="Africa CDC Program Lead,"
+          organization="Pathogen Genomics"
+        />
+      <TextSectionFive/>
+      <div className="w-full flex justify-center items-center">
+        
+      <div className="w-[80%] py-8">
+        <MainHeading>References</MainHeading>
+      <ReferenceList items={references} className="w-full"></ReferenceList>
       </div>
-
-      <div className="bg-gradient-to-t from-[#1E2859] to-[#1D5539] w-full h-auto flex items-center justify-center py-10">
-      <div className="w-[80%] flex flex-col lg:flex-row justify-center items-center ">
-         <div className="w-[80%] ">
-           <AnimatedHeading linecolor="white" fontSize="20px" className="text-white w-[60%]">
-             INCREASED AFRICAN SURVEILLANCE EFFORTS DEMONSTRATE FERTILE GROUND FOR INVESTING IN CONTINENTAL GENOMIC SEQUENCING CAPACITY-BUILDING
-           </AnimatedHeading>
-         </div>
-         <div>
-          <p className="text-white">Regular coordination between the Africa CDC and WHO has also helped optimize supply chains for critical laboratory resources like reagents, maximizing the network’s efficiency. For example, the JEAP partnership combined reagents delivered in an Africa CDC starter pack with supplementary reagents from WHO to enable the testing of a broader array of samples.</p>
-         </div>
-       </div>
       </div>
-
       <Footer />
     </div>
   );
