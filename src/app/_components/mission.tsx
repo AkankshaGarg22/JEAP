f391c8f4b5c79d524e6b664281310ce76529fcc9
dharm <singhdharm1008@gmail.com>
2025-06-15 14:24:17 +0530

    issue1

--- src/app/_components/mission.tsx
+++ src/app/_components/mission.tsx
@@ -4,12 +4,16 @@ import Typewriter from "typewriter-effect";
 
 const Mission = () => {
   return (
-    <div id="mission" className="h-[720px] xl:h-[1024px] flex flex-col items-center justify-start xl:justify-center bg-cover bg-center lg:bg-contain bg-no-repeat  bg-mb-curve md:bg-lg-curve z-[200]">
-      <div className="basis-2/3 md:basis-2/3 flex flex-col items-center justify-center md:justify-center xl:justify-center md:mt-4 gap-2 xl:gap-2 text-center text-white md:w-3/4 md:pt-4 ">
-        <h2 className="leading-1 xl:leading-[1.5] text-5xl md:text-[4rem] xl:text-7xl font-[arial-bold]">THE JEAP MISSION</h2>
+    <div id="mission" className="h-[720px] lg:h-[1048px] xl:h-[1056px] flex flex-col items-center justify-start xl:justify-center bg-cover bg-center lg:bg-contain bg-no-repeat  bg-mb-curve md:bg-lg-curve z-[200]">
+      <div className="basis-2/3 md:basis-2/3 flex flex-col items-center justify-center md:justify-center xl:justify-center md:mt-4 lg:mt-16 xl:mt-16 gap-2 xl:gap-2 text-center text-white md:w-4/5 xl:w-[90%] md:pt-4 ">
+        <h2 className="leading-1 xl:leading-[1.5] text-5xl md:text-[4rem] lg:text-[4.25rem] xl:text-7xl font-[arial-bold]">THE JEAP MISSION</h2>
         <div className="flex flex-col items-center gap-2 md:gap-8 xl:gap-4 text-lg xl:text-2xl md:text-xl px-4">
-          <p className="text-center xl:w-4/5 xl:leading-snug">Africa faces over a hundred health emergencies annually due to vulnerable health systems, hosting nearly a third of the world's refugee population.</p><p className="text-center xl:w-4/5 xl:leading-snug">
-          Despite these challenges, there is a tremendous opportunity to strengthen health systems.</p> <p className="text-center xl:w-4/5 xl:leading-snug"> With support from partners to build resilience, African countries can transform from being vulnerable to health emergencies to becoming models of stability and growth.
+          <p className="text-center xl:w-4/5 xl:leading-snug">
+            Africa faces over a hundred health emergencies annually due to vulnerable health systems, hosting nearly a third of the world's refugee population.
+          </p>
+          <p className="text-center xl:w-4/5 xl:leading-snug">Despite these challenges, there is a tremendous opportunity to strengthen health systems.</p>{" "}
+          <p className="text-center xl:w-4/5 xl:leading-snug">
+            With support from partners to build resilience, African countries can transform from being vulnerable to health emergencies to becoming models of stability and growth.
           </p>
         </div>
       </div>
